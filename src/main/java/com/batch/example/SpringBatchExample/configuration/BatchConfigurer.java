06cf5e28ff90e548e3b38ea0d233a22a93c86594
nileshjorwar <jorwarnilesh@gmail.com>
2021-05-20 08:47:57 -0500

    added tasklet

--- src/main/java/com/batch/example/SpringBatchExample/configuration/BatchConfigurer.java
+++ src/main/java/com/batch/example/SpringBatchExample/configuration/BatchConfigurer.java
@@ -2,6 +2,8 @@ package com.batch.example.SpringBatchExample.configuration;
 
 import com.batch.example.SpringBatchExample.entity.Contract;
 import com.batch.example.SpringBatchExample.entity.ContractHistory;
+import com.batch.example.SpringBatchExample.tasklets.CsvReaderTasklet;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,19 +14,22 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @Slf4j
 public class BatchConfigurer extends DefaultBatchConfigurer {
 
     @Bean
-    public Job startJob(JobBuilderFactory jobBuilderFactory, Step step1) {
+    public Job startJob(JobBuilderFactory jobBuilderFactory, Step step1, Step loadCsvToDBStep) {
         return jobBuilderFactory.get("contractEffective")
                 .incrementer(new RunIdIncrementer())
-                .start(step1)
-
+                .start(loadCsvToDBStep)
+                .next(step1)
                 .build();
     }
 
@@ -33,7 +38,6 @@ public class BatchConfigurer extends DefaultBatchConfigurer {
                       ItemReader<Contract> itemReader,
                       ItemProcessor<Contract, ContractHistory> itemProcessor,
                       ItemWriter<ContractHistory> itemWriter) throws Exception {
-//        log.info("Holder from: " + itemReader.read().getHolderName());
         return stepBuilderFactory.get("step1")
                 .<Contract, ContractHistory>chunk(500)
                 .reader(itemReader)
@@ -41,4 +45,12 @@ public class BatchConfigurer extends DefaultBatchConfigurer {
                 .writer(itemWriter)
                 .build();
     }
+
+    @Bean
+    public Step loadCsvToDBStep(StepBuilderFactory stepBuilderFactory) throws Exception {
+        return stepBuilderFactory.get("load-Csv-To-DB")
+                .tasklet(new CsvReaderTasklet())
+                .build();
+    }
+
 }
